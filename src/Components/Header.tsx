70ac53f69a2f83233d5df1bea5b34f3f7a5b1a02
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2022-09-17 16:42:01 +0300

    first commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src='https://assets.stickpng.com/thumbs/58afdf94829958a978a4a6bd.png'/>
+        </header>
+    )
+}
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img className="img-forest"
+                     src='https://abrakadabra.fun/uploads/posts/2022-01/1643659835_9-abrakadabra-fun-p-dlinnii-fon-lesa-17.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
