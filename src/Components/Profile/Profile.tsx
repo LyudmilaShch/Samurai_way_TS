945cbcd2bbfc2fc15e0336da1d3b18f6154bb484
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2022-09-23 22:42:11 +0300

    18 lesson - Dialogs

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img className={s.img_forest}
                      src='https://abrakadabra.fun/uploads/posts/2022-01/1643659835_9-abrakadabra-fun-p-dlinnii-fon-lesa-17.jpg'/>
