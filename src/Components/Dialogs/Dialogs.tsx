93499b91ac6e8b3d2af36fccaed097ec036493bf
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2022-09-24 23:54:19 +0300

    21 lesson - dialogs

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -7,8 +7,28 @@ type DialogsType = {
 
 export const Dialogs = (props: DialogsType) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+           <div className={s.dialogsItems}>
+               <div className={s.dialog}>
+                   Rabbit
+               </div>
+               <div className={s.dialog}>
+                   Leo
+               </div>
+               <div className={s.dialog}>
+                   Mouse
+               </div>
+               <div className={s.dialog}>
+                   Wolf
+               </div>
+           </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your IT</div>
+                <div className={s.message}>Yo</div>
+
+
+            </div>
         </div>
     )
 }
