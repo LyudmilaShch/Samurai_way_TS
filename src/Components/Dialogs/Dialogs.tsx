84c2aa0d97041a5729d939cc09b680a1727742d2
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2022-09-27 23:27:33 +0300

    24 lesson - UI - BLL, выносим данные в сторону

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -16,7 +16,7 @@ const DialogItem = (props: DialogItemType) => {
     let path = "/dialogs/" + props.id;
 
     return <div className={s.dialog + ' ' + s.active}>
-    <NavLink to = {path}>{props.name}</NavLink>
+        <NavLink to={path}>{props.name}</NavLink>
     </div>
 }
 
@@ -25,22 +25,40 @@ const Message = (props: MessageType) => {
 }
 
 export const Dialogs = (props: DialogsType) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Rabbit'},
+        {id: 2, name: 'Leo'},
+        {id: 3, name: 'Mouse'},
+        {id: 4, name: 'Wolf'},
+        {id: 5, name: 'Mouse'},
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your IT'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'},
+    ]
+
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                 <DialogItem name={"Rabbit"} id={1}/>
-                 <DialogItem name={"Leo"} id={2}/>
-                 <DialogItem name={"Mouse"} id={3}/>
-                 <DialogItem name={"Wolf"} id={4}/>
-                 <DialogItem name={"Mouse"} id={5}/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+
             </div>
             <div className={s.messages}>
-                <Message message={"Hi"}/>
-                <Message message={"How is your IT"}/>
-                <Message message={"Yo"}/>
-                <Message message={"Yo"}/>
-                <Message message={"Yo"}/>
-
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
+                <Message message={messagesData[3].message}/>
+                <Message message={messagesData[4].message}/>
             </div>
         </div>
     )
--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -3,9 +3,38 @@ import s from './MyPosts.module.css';
 import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
+
+    let PostsData = [
+        {
+            id: 1,
+            message: "Fur-fur-fur",
+            avatar: "https://oir.mobi/uploads/posts/2021-04/1619183869_43-oir_mobi-p-khitraya-lisa-zhivotnie-krasivo-foto-49.jpg",
+            countlike: 5
+        },
+        {
+            id: 2,
+            message: "Keooo-keooo",
+            avatar: "http://simple-fauna.ru/wp-content/uploads/2018/06/belki.jpg",
+            countlike: 7
+        },
+        {
+            id: 3,
+            message: "R-r-r-r-r-r",
+            avatar: "https://vsezhivoe.ru/wp-content/uploads/2017/10/lev-14851893401673.jpg",
+            countlike: 15
+        },
+        {
+            id: 4,
+            message: "I am deer",
+            avatar: "https://www.kalashnikov.ru/wp-content/uploads/2021/04/natsionalnyi-park-ssha-priroda-1024x1024.jpg",
+            countlike: 12
+        },
+
+    ]
+
     return (
         <div className={s.postsBlock}>
-           <h3>My Posts</h3>
+            <h3>My Posts</h3>
             <div>
                 <div>
                     <textarea></textarea>
@@ -15,16 +44,18 @@ export const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={"Fur-fur-fur"}
-                      avatar={"https://oir.mobi/uploads/posts/2021-04/1619183869_43-oir_mobi-p-khitraya-lisa-zhivotnie-krasivo-foto-49.jpg"}
-                      countlike={5}/>
-                <Post message={"Keooo-keooo"} avatar={"http://simple-fauna.ru/wp-content/uploads/2018/06/belki.jpg"}
-                      countlike={7}/>
-                <Post message={"R-r-r-r-r-r"}
-                      avatar={"https://vsezhivoe.ru/wp-content/uploads/2017/10/lev-14851893401673.jpg"} countlike={15}/>
-                <Post message={"I am deer"}
-                      avatar={"https://www.kalashnikov.ru/wp-content/uploads/2021/04/natsionalnyi-park-ssha-priroda-1024x1024.jpg"}
-                      countlike={12}/>
+                <Post message={PostsData[0].message}
+                      avatar={PostsData[0].avatar}
+                      countlike={PostsData[0].countlike}/>
+                <Post message={PostsData[1].message}
+                      avatar={PostsData[1].avatar}
+                      countlike={PostsData[1].countlike}/>
+                <Post message={PostsData[2].message}
+                      avatar={PostsData[2].avatar}
+                      countlike={PostsData[2].countlike}/>
+                <Post message={PostsData[3].message}
+                      avatar={PostsData[3].avatar}
+                      countlike={PostsData[3].countlike}/>
             </div>
         </div>
     )
