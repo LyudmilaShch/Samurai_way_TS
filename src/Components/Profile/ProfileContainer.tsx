27e45ed549affa6fcb759b45b5ba215fada17ab1
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2022-12-23 23:32:11 +0300

    83 - подключаем reselect

--- src/Components/Profile/ProfileContainer.tsx
+++ src/Components/Profile/ProfileContainer.tsx
@@ -65,6 +65,5 @@ let mapStateToProps = (state: AppStateType): MapStatePropsType => {
 export default compose<React.ComponentType>(
     connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
     withRouter,
-    //withAuthRedirect
 )(ProfileContainer)
 
